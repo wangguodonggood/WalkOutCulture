68a20d6ea09ef647b9bc3952a5a1f02b58bde2d2
wgdhub <1350802989@qq.com>
2018-02-13 17:57:08 +0800

    增加TestGit类

---
+++ 出走文化/src/main/java/com/shiyuesoft/util/TestGit.java
@@ -0,0 +1,14 @@
+package com.shiyuesoft.util;
+
+import android.util.Log;
+
+/**
+ * Created by wangg on 2018/2/13.
+ */
+public class TestGit {
+
+    public void test(){
+        Log.i("test", "test: "+"hello git");
+    }
+
+}
